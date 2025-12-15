816d5e60e6dfff888854376bf1601291fd0a9ff3
arnoldd2 <androapps2030@gmail.com>
2025-12-15 11:21:08 +0500

    Create search.js

---
+++ search.js
@@ -0,0 +1,11 @@
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("input", function () {
+  const value = this.value.toLowerCase();
+
+  cards.forEach(card => {
+    const title = card.dataset.title.toLowerCase();
+    card.style.display = title.includes(value) ? "" : "none";
+  });
+});
