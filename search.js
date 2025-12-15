1d1391aad060b76d8795d3c710205b52485634aa
arnoldd2 <androapps2030@gmail.com>
2025-12-15 11:23:08 +0500

    Update search.js

--- search.js
+++ search.js
@@ -1,11 +1,13 @@
 const searchInput = document.getElementById("searchInput");
 const cards = document.querySelectorAll(".card");
 
-searchInput.addEventListener("input", function () {
-  const value = this.value.toLowerCase();
+if (searchInput) {
+  searchInput.addEventListener("input", function () {
+    const value = this.value.toLowerCase();
 
-  cards.forEach(card => {
-    const title = card.dataset.title.toLowerCase();
-    card.style.display = title.includes(value) ? "" : "none";
+    cards.forEach(card => {
+      const title = (card.dataset.title || "").toLowerCase();
+      card.style.display = title.includes(value) ? "" : "none";
+    });
   });
-});
+}
